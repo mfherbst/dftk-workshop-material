e041fc563dbb9d9a52493e6990e710181ec2c20b
Michael F. Herbst <info@michael-herbst.com>
2022-09-12 12:56:17 +0200

    Install ASE as well

--- install-manual.jl
+++ install-manual.jl
@@ -6,4 +6,20 @@ flush(stdout)
 Pkg.instantiate()
 Pkg.precompile()
 
+using PyCall
+using Conda
+if ispynull(pyimport_e("ase"))
+    if !PyCall.conda
+        println("Not installing ASE albeit missing. Please install ASE manually.")
+        println("To do so, most likely you want to run the following command:")
+        python = PyCall.python
+        println("     $(python) -m pip install ase")
+    else
+        println("Installing ASE to Julia built-in Conda environment")
+        Conda.add("ase", channel="conda-forge")
+    end
+else
+    println("Found ASE ... not installing.")
+end
+
 println("Done!")
--- install.jl
+++ install.jl
@@ -12,6 +12,31 @@ success = WorkshopWizard.install(;repo="https://github.com/mfherbst/dftk-worksho
                                   global_IJulia=true,
                                   auto_overwrite=false)
 
+repo = joinpath(path, "dftk-workshop-material")
 if success
-    @info "That's it. Start the notebook server with 'using IJulia; notebook(dir=\"$path/dftk-workshop-material\")' ..."
+    Pkg.activate(repo)
+    using PyCall
+    using Conda
+    using REPL.TerminalMenus
+
+    if ispynull(pyimport_e("ase"))
+        if !PyCall.conda
+            println("The python dependency ASE is missing and needs to be installed.")
+            println("To do so, most likely you need to run the following command:")
+            python = PyCall.python
+            println("     $(python) -m pip install ase")
+            @info "Do you want me to run this command right now?"
+            selected_yes = request("", RadioMenu(["yes", "no"], pagesize=2)) == 1
+            if selected_yes
+                run(`$(python) -m pip install ase`)
+            end
+        else
+            println("Installing ASE to Julia built-in Conda environment")
+            Conda.add("ase", channel="conda-forge")
+        end
+    else
+        println("Found ASE ... not installing.")
+    end
+
+    @info "That's it. Start the notebook server with 'using IJulia; notebook(dir=\"$repo\")' ..."
 end
